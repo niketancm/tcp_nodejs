545ddf1a754a3c90c966942b03760ab57d6884e7
niketan <niketan@gmail.com>
2018-05-18 11:30:18 +0530

    inserting data into the database

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
 #!/usr/bin/env node
 'use strict';
 //for data to be pushed into database
-const models = require('./data-models/models');
+// const models = require('./data-models/models');
+const mongoose = require('mongoose');
+
+//connect to the database
+mongoose.connect('mongodb://localhost:27017/myTestDB`');
+
+var db = mongoose.connection;
+
+//check the connection to mongodb
+db.on('error', function (err) {
+    console.log('connection error', err);
+    });
+    
+    db.once('open', function () {
+    console.log('connected.');
+    });       
+
+//define a schema
+var Schema = mongoose.Schema;
+
+var dataSchema = new Schema ({
+    // DATETIME: Date,
+    // REGION: String,
+    // LOCATION: String,
+    // PLANT: String,
+    // LINE: String,
+    // MODEL: String,
+    // OPERATOR: String,
+    // DEVICEID: String,
+    PARAMATER: String,
+    DATA: Number
+});
+//define the model for the schema
+mongoose.model('data', dataSchema);
+
+//configure the data model    
+// var dataModel = mongoose.model('dataModel',     dataSchema);
 
 // load the Node.js TCP library
 const net = require('net');
@@ -26,25 +62,15 @@ function onClientConnected(socket) {
       // getting the string message and also trimming
       // new line characters [\r or \n]
       let m = data.toString().replace(/[\n\r]*$/, '');
-
+        var Data = mongoose.model('data');
+        const dataInsert = new Data;
       // split the message
       var incomingData = m.split(',');
-      incomingData[9] = parseFloat(incomingData[9]);
+    //   incomingData[9] = parseFloat(incomingData[9]);
+      dataInsert.PARAMATER = incomingData[0];
+      dataInsert.data = data[1];
 
-      var dataWrite = new dataModel({
-          DATETIME: incomingData[0],
-          REGION: incomingData[1],
-          LOCATION: incomingData[2],
-          PLANT: incomingData[3],
-          LINE: incomingData[4],
-          MODEL: incomingData[5],
-          OPERATOR: incomingData[6],
-          DEVICEID: incomingData[7],
-          PARAMATER: incomingData[8],
-          DATA: incomingData[9]
-      });
-
-      incomingData.save(function(error){
+      dataInsert.save(function(error){
           console.log("Your data has been saved!");
           if(error) {
               console.error(error);
