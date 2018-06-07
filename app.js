e01d72403727ea57a35fcbf998908d7caf5aaac8
Niketan <niketancm@gmail.com>
2018-06-07 10:09:21 +0530

    tcp accepting both req to stream to clients and data from iot

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ var dataSchema = new Schema ({
     MODEL: String,
     OPERATOR: String,
     DEVICEID: String,
-    PARAMATER: String,
+    PARAMETER: String,
     DATA: Number
 });
 
@@ -53,67 +53,69 @@ function onClientConnected(socket) {
     // let clientName = `${socket.remoteAddress}:${socket.remotePort}`;
     // Logging the message on the server
     console.log(`${clientName} connected.`);
+    socket.write("101")
 
     // Triggered on data received by this client
     socket.on('data', (data) => {
-    let clientName = `${socket.remoteAddress}:${socket.remotePort}`;
-        
+        let clientName = `${socket.remoteAddress}:${socket.remotePort}`;
+        // getting the string message and also trimming
+        // new line characters [\r or \n]
+        let m = data.toString().replace(/[\n\r]*$/, '');
+        // split the message
+        var incomingData = m.split(',');
+        if(incomingData[0] === streamId){ //this is a req conn to stream
+            if(!streamReq[clientName]){//new connection
+                //register the scoket as a key value pair, key: clientname and value: socket
+                streamReq[clientName] = socket;
+            }else{//connection already present
+                return;
+            }
+        }else{//not stream id, these are iot connections
+            if(!iotSockets[clientName]){//new iot connectons
+                //register the scoket as a key value pair, key: clientname and value: socket
+                iot[clientName] = socket;
+            }else{//iot connections already there, insert data
 
+                //define a model dataModel
+                var Data = mongoose.model('dataModel',dataSchema);
+                const dataInsert = new Data;
+                // for all the 10 input parameters
+                //incomingData[9] = parseFloat(incomingData[9]);
+                //for 9 inputs not considering date
+                incomingData[8] = parseFloat(incomingData[8]);
+                //for just the two elements, last data is float type
+                //incomingData[1] = parseFloat(incomingData[1]);
 
-
-
-
-
-      // getting the string message and also trimming
-      // new line characters [\r or \n]
-      let m = data.toString().replace(/[\n\r]*$/, '');
-
-      //define a model dataModel
-      var Data = mongoose.model('dataModel', dataSchema);
-      const dataInsert = new Data;
-      
-      // split the message
-      var incomingData = m.split(',');
-      
-      // for all the 10 input parameters
-      //incomingData[9] = parseFloat(incomingData[9]);
-      //for 9 inputs not considering date
-      incomingData[8] = parseFloat(incomingData[8]);
-      
-      
-      //for just the two elements, last data is float type
-    //   incomingData[1] = parseFloat(incomingData[1]);
-
-      //save the incoming data to the mongoose model to be inserted
-      dataInsert.REGION = incomingData[0];
-      dataInsert.LOCATION = incomingData[1];
-      dataInsert.PLANT = incomingData[2];
-      dataInsert.LINE = incomingData[3];
-      dataInsert.MODEL= incomingData[4];
-      dataInsert.OPERATOR = incomingData[5];
-      dataInsert.DEVICEID = incomingData[6];
-      dataInsert.PARAMATER = incomingData[7];
-      dataInsert.DATA = incomingData[8];
-
-    //   console.log(dataInsert.PARAMATER);
-    //   console.log(dataInsert.DATA);
-
-      dataInsert.save(function(error){
-          if(error) {
-            //   console.error(error);
-            console.log('Send the data in correct format');
-          }else{
-            console.log("Your data has been saved!");
-          }
-      });
-
-      // Logging the message on the server
-      console.log(`${clientName} said: ${m}`);
-
-      // notifing the client
-      socket.write(`We got your message (${m}). Thanks!\n`);
+                //save the incoming data to the mongoose model to be inserted
+                dataInsert.REGION = incomingData[0];
+                dataInsert.LOCATION = incomingData[1];
+                dataInsert.PLANT = incomingData[2];
+                dataInsert.LINE = incomingData[3];
+                dataInsert.MODEL= incomingData[4];
+                dataInsert.OPERATOR = incomingData[5];
+                dataInsert.DEVICEID = incomingData[6];
+                dataInsert.PARAMETER = incomingData[7];
+                dataInsert.DATA = incomingData[8];
+                //console.log(dataInsert.PARAMATER);
+                //console.log(dataInsert.DATA);
+                dataInsert.save(function(error){
+                    if(error) {
+                      //   console.error(error);
+                      console.log('Send the data in correct format');
+                    }else{
+                      console.log("Your data has been saved!");
+                    }
+                });
+                // Logging the message on the server
+                console.log(`${clientName} said: ${m}`);
+                //Send the data to the clients in reqSockets
+                // socket.write(`We got your message (${m}). Thanks!\n`);
+                Object.entries(streamReq).forEach(([key, cs]) => {
+                    cs.write(incomingData[8]);
+                });
+            }
+        }
     });
-
     // Triggered when this client disconnects
     socket.on('end', () => {
         // Logging this message on the server
--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const { createServer } = require('net');
 
 const server = createServer();
 
-// let counter = 0;
+let counter = 0;
 const sockets = {};
-var id = "";
-const inputSockets = {};
-const outputSockets = {};
+// var id = "";
+// const inputSockets = {};
+// const outputSockets = {};
 
 server.on('connection', (socket) => {
-  // socket.id = counter++
-  // sockets[socket.id] = socket;
+  socket.id = counter++;
+  sockets[socket.id] = socket;
   socket.write("A client has connected\n");
   // console.log(sockets[counter - 1].id);
   console.log("\n");
 
   socket.on('data', data => {
     var arr = Object.entries(sockets);
-    // console.log(sockets);
+    console.log(sockets);
     console.log("hello WRITING");    
     // console.log(arr);
     arr.forEach(([key, cs]) => {
