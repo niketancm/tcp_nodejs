de80ec060312094d149c0011114a36ca7bf441d8
niketan <niketan@gmail.com>
2018-06-15 11:33:56 +0530

    This spliting the message and display the result- seperate.js

--- seperateData.js
+++ seperateData.js
@@ -1,6 +1,7 @@
 var speratedArray = [];
-var message = "##ttkId10, India, Bangalore, ttk_magadi, line1, d316, shankar, I0019, temperature, 23.04"//+ "##";
-var message2 = "##ttkId10, India, Chennai, ttk_hosur, line1, d316, shankar, I0019, temperature, 23.04" //+ "##";
+var message = "ttkId10, India, Bangalore, ttk_magadi, line1, d316, shankar, I0019, temperature, 23.04" + "##";
+var message2 = "ttkId10, India, Chennai, ttk_hosur, line1, d316, shankar, I0019, temperature, 23.04" + "##";
+var message3 = "ttkId10, India, Pune, ttk_shaniwarWada, line1, d316, shankar, I0019, temperature, 23.04" + "##";
 speratedArray = message.split(',');
 // // console.log(speratedArray[9]);
 // // console.log(parseFloat(speratedArray[9]));
@@ -25,12 +26,16 @@ speratedArray = message.split(',');
 // console.log(typeof(persons));
 // console.log(persons.has('niketan'));
 // console.log(persons.remove(name1));
-finalmsg = message + message2;
+var spliMsg = [];
+var finalMsg = [];
+finalmsg = message + message2 + message3; 
 spliMsg = finalmsg.split('##');
 // console.log(finalmsg);
-// console.log(spliMsg +" length: " + spliMsg.length );
-for( var i = 0; i < spliMsg.length; i++){
-    if(!spliMsg[i]){
-        console.log(spliMsg[i] +" length: " + spliMsg.length + " " + i);        
-    }
+console.log(" length: " + spliMsg.length );
+for( var i = 0; i < (spliMsg.length - 1); i++){    
+        // console.log(spliMsg[i] +" length: " + spliMsg.length + " " + i);        
+        // console.log(spliMsg[i]);                
+        finalMsg.push(spliMsg[i])
 }
+console.log("Final message: \n");
+console.log(finalMsg + "LEngth is: " + finalMsg.length);
