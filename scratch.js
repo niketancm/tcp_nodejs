f853f71f65dd785d70ffa734849aa1b93cb17022
Niketan <niketancm@gmail.com>
2018-06-20 13:40:14 +0530

    renamed seperatedata.js file and cleaned few unwanted files

--- scratch.js
+++ scratch.js
@@ -40,3 +40,21 @@ for( var i = 0; i < (spliMsg.length - 1); i++){
 }
 console.log("Final message: \n");
 console.log(finalMsg + "LEngth is: " + finalMsg.length);
+
+var message = "ttk, devideId, line1, unit-no1, operation1, thickness=0.10, height=10, weight=100";
+var othervalue = message.split(',');
+// message2 = "thickness=0.10"
+// parameter = message2.split('=')
+var myObj = {
+    device: othervalue[1],
+    line: othervalue[2],
+    unit: othervalue[3],
+    operation: othervalue[4]
+};
+for(var i = 5; i < othervalue.length; i++){
+    para = othervalue[i].split('=');
+    myObj[para[0]] = parseFloat(para[1]);
+}
+
+
+console.log(myObj);
